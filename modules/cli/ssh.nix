45201698b375ac7838a886121014467294dcf17a
Jaden Nola <proto@jadestar.dev>
2025-12-22 13:10:41 -0800

    chore: clean up secrets

--- modules/cli/ssh.nix
+++ modules/cli/ssh.nix
@@ -1,15 +1,6 @@
 {
   unify.modules.dev = {
-    home = {
-      config,
-      pkgs,
-      ...
-    }: {
-      sops.secrets = {
-        "ssh_mma_github_rw_private_key" = {
-          path = "${config.home.homeDirectory}/.ssh/id_mma_github_rw";
-        };
-      };
+    home = {pkgs, ...}: {
       programs.ssh = {
         enable = true;
         enableDefaultConfig = false;
@@ -34,14 +25,6 @@
               user = "git";
               identitiesOnly = true;
               identityFile = "${pkgs.writeText "id_gh_access.pub" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOh1Nd4Va9xCaEb4evaiclAiHMX6aX8+vXgf+AzlTfbe"}";
-            };
-          "gh-mma" =
-            defaults
-            // {
-              hostname = "github.com";
-              user = "git";
-              identitiesOnly = true;
-              identityFile = config.sops.secrets."ssh_mma_github_rw_private_key".path;
             };
         };
       };
