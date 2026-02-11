fc16f1b3d284b970deb2c657cd2e231c80dc146b
Jaden Nola <proto@jadestar.dev>
2026-02-11 12:07:55 -0600

    fix: wait for /data to mount before starting relevant containers

--- hosts/aesop/external-ssd.nix
+++ hosts/aesop/external-ssd.nix
@@ -14,5 +14,25 @@
     environment.etc.crypttab.text = ''
       media UUID=a0ae2a60-be3f-45b6-b410-96bf0065bc30 /root/external_ssd.key nofail
     '';
+
+    # Containers that need to wait for /data to mount
+    systemd.services = let
+      mediaContainers = [
+        "jellyfin"
+        "qbittorrent"
+        "sonarr"
+        "radarr"
+        "cross-seed"
+        "decluttarr"
+        "bazarr"
+      ];
+    in
+      builtins.listToAttrs (map (x: {
+          name = "podman-${x}";
+          value = {
+            unitConfig.RequiresMountsFor = ["/data"];
+          };
+        })
+        mediaContainers);
   };
 }
