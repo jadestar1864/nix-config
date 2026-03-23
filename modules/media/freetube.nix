4a75cf88856ae0c51493a6ec91a30c35c2110c06
Jaden Nola <proto@jadestar.dev>
2026-03-23 02:52:30 -0500

    feat: use flatpak freetube for temp

--- modules/media/freetube.nix
+++ modules/media/freetube.nix
@@ -1,7 +1,9 @@
 {
   unify.modules.pc.home = {pkgs, ...}: {
-    home.packages = with pkgs; [
-      freetube
-    ];
+    # TODO: Check if freetube is updated past 0.23.15
+    #home.packages = with pkgs; [
+    #  freetube
+    #];
+    services.flatpak.packages = ["io.freetubeapp.FreeTube"];
   };
 }
