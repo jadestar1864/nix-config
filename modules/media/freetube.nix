f7aa4edbaae70067d0ddce3f4efc065c98efcd04
Jaden Nola <proto@jadestar.dev>
2026-03-29 23:21:48 -0500

    fix: use freetube from nixpkgs

--- modules/media/freetube.nix
+++ modules/media/freetube.nix
@@ -1,9 +1,7 @@
 {
   unify.modules.pc.home = {pkgs, ...}: {
-    # TODO: Check if freetube is updated past 0.23.15
-    #home.packages = with pkgs; [
-    #  freetube
-    #];
-    services.flatpak.packages = ["io.freetubeapp.FreeTube"];
+    home.packages = with pkgs; [
+      freetube
+    ];
   };
 }
